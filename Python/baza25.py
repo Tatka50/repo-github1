d667226da22ab000a78bf734975135e157d48378
Tato <komissarovat8@gmail.com>
2020-11-04 18:30:31 +0300

    tele93 и baza25 работают без таблицы usrers

---
+++ Python/baza25.py
@@ -0,0 +1,65 @@
+global kud
+global otk
+global usi
+global text
+global name
+
+import mysql.connector
+from mysql.connector import Error
+
+
+import mysql.connector
+def baza (kud,otk,usi,text,name):
+  conn = mysql.connector.connect(
+         user='root',
+         password='lizard',
+         host='localhost',
+         database='tele')
+
+
+  cur = conn.cursor()
+
+  print('id ользователя=',usi)
+  print('kud в базе=',kud)
+  print('otk в базе=',otk)
+  print('text в базе  ',text)
+  
+  #us=str(usi)
+  #print('us=',us)
+  #cur = conn.cursor()
+  #query = "SELECT  * FROM users WHERE usid LIKE \"%" + us+"%\""
+  
+  #cur.execute(query,us)
+  #data = cur.fetchall()
+  #print(data)
+  #nam=data[0]
+  #nam1=nam[0]
+
+  #print(data)
+  #print("полученное из базы имя", nam1)
+
+  
+  
+  query = "INSERT INTO posts(usid,id_gr_otk,id_gr_kud,post,naneusid) VALUES(%s,%s,%s,%s,%s)"
+  args = (usi,otk,kud,text,name)
+  print(args)
+  cur.execute(query,args)
+
+
+
+
+  conn.commit()  
+
+
+
+
+
+
+
+  conn.close()
+ 
+
+
+
+  conn.close()
+ 
---
+++ Python/tele93.py
@@ -0,0 +1,89 @@
+global kud
+global otk
+global usi 
+global text
+global name
+
+kud=0
+otk=0
+usid=0
+import telebot
+#import configparser
+import json
+import requests
+import mysql.connector
+from mysql.connector import Error
+from baza25 import baza
+
+conn = mysql.connector.connect(
+         user='root',
+         password='lizard',
+         host='localhost',
+         database='tele')
+
+
+
+
+
+
+Token1="948381879:AAEO07_DU1z-CO67NRWIi8HX_jeLNfQVqPo"
+bot = telebot.TeleBot(Token1)
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    global otk
+    global usi
+    user_id = message.from_user.id
+    #работа с ьазой
+    print('user_id=',user_id)
+    bot.send_message( message.chat.id, "выберите чат для пересылки")
+    print('message.chat.id1=',message.chat.id)
+    otk=message.chat.id
+    print('otk= ',otk)
+    usi=user_id
+@bot.message_handler(commands=['help'])
+def start_command(message):
+  global kud
+  global name
+  bot.send_message( message.chat.id,  "Хорошо")
+  print('message.chat.id=', message.chat.id)
+  kud=message.chat.id
+  name=message.from_user.username
+  print('kud=',kud)
+  print('name=',name)
+  
+@bot.message_handler(content_types=['text'])
+def repeat_all_messages(message):
+   global otk
+   global text
+   global name
+   indikator=0
+   print ('kudprom=',kud)
+   if (message.chat.id!=kud):print( message.chat.id)
+   else: print ('kudprom=',kud)
+   if (message.chat.id!=kud):
+       text= message.text
+       if message.text[0]=="#":                          
+            bot.forward_message(kud ,otk, message.message_id)
+            print('newkud=',kud)
+            print('newotk=', otk)
+            indikator=1
+            #запись в базу
+            print('kud=',kud)
+            print('otk=', otk)
+            print('usi=',usi)
+            print('text=',text)
+            print('name=',name)
+            baza(kud,otk,usi,text,name)
+
+
+
+
+            
+   else :
+       print("пишите в чат где старт")
+       bot.send_message( kud,  "пишите в чат где start")
+
+
+
+bot.polling()
+
