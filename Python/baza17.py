d08465090c2f7474353803461db4408057726291
Tato <komissarovat8@gmail.com>
2020-11-04 11:36:54 +0300

    изменение tele17 и tele90

--- Python/baza17.py
+++ Python/baza17.py
@@ -8,24 +8,37 @@ from mysql.connector import Error
 
 
 import mysql.connector
-def baza (usi,kud,otk,text):
+def baza (kud,otk,usi,text):
   conn = mysql.connector.connect(
          user='root',
          password='lizard',
          host='localhost',
          database='tele')
 
-
+  print('id ользователя=',usi)
+  print('kud в базе=',kud)
+  print('otk в базе=',otk)
+  print('text в базе  ',text)
+  
+  us=str(usi)
+  print('us=',us)
   cur = conn.cursor()
-  query = ("SELECT n * FROM users WHERE usid=usi")
-  n1=n
+  query = "SELECT  * FROM users WHERE usid LIKE \"%" + us+"%\""
+  
+  cur.execute(query,us)
+  data = cur.fetchall()
+  print(data)
+  nam=data[0]
+  nam1=nam[0]
 
+  print(data)
+  print("полученное из базы имя", nam1)
 
-
-
+  
+  
   query = "INSERT INTO posts(usid,id_gr_otk,id_gr_kud,post,naneusid) VALUES(%s,%s,%s,%s,%s)"
-  args = (usi,otk,kud,text,n1)
-#print(args)
+  args = (usi,otk,kud,text,nam1)
+  print(args)
   cur.execute(query,args)
 
 
